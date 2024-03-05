78565db5bd585c36f04027ad01dc2b95371d9d36
NortinPowers <a.nortin@gmail.com>
2024-03-05 10:44:42 +0300

    JavaDoc

--- spring-boot-logger-aspect-starter/src/main/java/by/clevertec/aspect/LoggingAspect.java
+++ spring-boot-logger-aspect-starter/src/main/java/by/clevertec/aspect/LoggingAspect.java
@@ -14,33 +14,62 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
 
+    /**
+     * Определяет точку среза для аспекта, связанного с аннотацией {@link ControllerAspectLogger}.
+     */
     @Pointcut("@annotation(ControllerAspectLogger)")
     public void callControllerAspectLoggerAnnotation() {
     }
 
+    /**
+     * Логирует запрос перед выполнением метода контроллера.
+     *
+     * @param joinPoint Точка соединения, представляющая вызов метода.
+     */
     @Before("callControllerAspectLoggerAnnotation()")
     public void logRequest(JoinPoint joinPoint) {
         log.info("Request: " + joinPoint.getSignature().toShortString());
     }
 
+    /**
+     * Логирует ответ после успешного выполнения метода контроллера.
+     *
+     * @param result Результат выполнения метода.
+     */
     @AfterReturning(pointcut = "callControllerAspectLoggerAnnotation()", returning = "result")
     public void afterReturningAdvice(Object result) {
         log.info("Response: " + result);
     }
 
+    /**
+     * Определяет точку среза для аспекта, связанного с аннотацией {@link ServiceAspectLogger}.
+     */
     @Pointcut("@annotation(ServiceAspectLogger)")
     public void callServiceAspectLoggerAnnotation() {
     }
 
+    /**
+     * Логирует исключение, возникшее в сервисном методе.
+     *
+     * @param exception Исключение, которое было выброшено.
+     */
     @AfterThrowing(pointcut = "callServiceAspectLoggerAnnotation()", throwing = "exception")
     public void logServiceException(Exception exception) {
         log.error("Unexpected error", exception);
     }
 
+    /**
+     * Определяет точку среза для аспекта, связанного с аннотацией {@link RepositoryAspectLogger}.
+     */
     @Pointcut("@annotation(RepositoryAspectLogger)")
     public void callRepositoryAspectLoggerAnnotation() {
     }
 
+    /**
+     * Логирует исключение, возникшее в методе репозитория.
+     *
+     * @param exception Исключение, которое было выброшено.
+     */
     @AfterThrowing(pointcut = "callRepositoryAspectLoggerAnnotation()", throwing = "exception")
     public void logRepositoryException(Exception exception) {
         log.error("Unexpected error", exception);
